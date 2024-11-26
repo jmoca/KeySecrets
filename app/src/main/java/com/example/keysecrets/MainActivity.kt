d92492e8be6b060ec29264cb06315633becdadd5
rafael <rcastanoblanca@safareyes.es>
2024-11-26 10:43:05 +0100

    vistas terminadas

---
+++ app/src/main/java/com/example/keysecrets/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.keysecrets
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContentView(R.layout.settings)
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        //    insets
+        //}
+    }
+}
